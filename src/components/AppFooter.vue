c2f2777d19caf125486b70e4845a8d126da78d1c
Vladislav-Ciuperca <ciuperca37@gmail.com>
2025-04-03 23:56:57 +0200

    base ok mancano cards

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,7 +3,57 @@
 export default {
     data() {
         return {
-            // placeholder
+            content: {
+                "element_1": {
+                    'pos_id':'left',
+                    'title': "DC COMICS",
+                    'values': [
+                        "Characters",
+                        "Comics",
+                        "Movies",
+                        "TV",
+                        "Games",
+                        "Videos",
+                        "News",
+                    ],
+                },
+                "element_2": {
+                    'pos_id':'left',
+                    'title': "SHOP",
+                    'values': [
+                        "Shop DC",
+                        "Shop DC collectibles",
+                    ],
+                },
+                "element_3": {
+                    'pos_id':'mid',
+                    'title': "DC ",
+                    'values': [
+                        "Terms Of Use",
+                        "Privacy Policy(New)",
+                        "Ad Choices",
+                        "Advertising",
+                        "Jobs",
+                        "Subscriptions",
+                        "talent Workshop",
+                        "CPSC Certificates",
+                        "Ratings",
+                        "Shop Help",
+                        "Contact Us",
+                    ],
+                },
+                "element_4": {
+                    'pos_id':'right',
+                    'title': "SITES ",
+                    'values': [
+                        "DC",
+                        "MAD magazines",
+                        "DC Kids",
+                        "DC Universe",
+                        "DC Power visa",
+                    ],
+                },
+            }
         }
     },
     methods: {
@@ -20,13 +70,125 @@ export default {
 
 <!-- ///////////////////////////////////////////// -->
 <template>
-    <div class="container">
-        <h1>=qui footger=</h1>
+    <div class="cont">
+        <div class="container">
+            <div class="left">
+
+                <div class="sec">
+                    <div class="title">
+                        DC COMICS
+                    </div>
+                    <div class="content">
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </div>
+
+                    <div class="title">
+                        SHOP
+                    </div>
+                    <div class="content">
+                        <li>Shop DC</li>
+                        <li>Shop DC collectibles</li>
+                    </div>
+                </div>
+
+                <div class="sec">
+                    <div class="title">
+                        DC
+                    </div>
+                    <div class="content">
+                        <li>Terms Of Use</li>
+                        <li>Privacy Policy(New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>talent Workshop</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </div>
+                </div>
+
+                <div class="sec">
+                    <div class="title">
+                        SITES
+                    </div>
+                    <div class="content">
+                        <li>DC</li>
+                        <li>MAD magazines</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power visa</li>
+                    </div>
+                </div>
+
+               
+
+                
+            </div>
+            <div class="right">
+                <!-- placeholder -->
+            </div>
+        </div>
     </div>
 </template>
 <!-- ///////////////////////////////////////////// -->
 
 
 <style scoped>
-/* plaecholder */
+
+.title{
+    font-size: 1.3rem;
+    font-weight: 500;
+    margin-top: 2rem;
+}
+
+.content{
+    color: #969696;
+    font-size: 15px;
+}
+li{
+    list-style-type: none;
+}
+.cont {
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-image: url(/src/assets/img/footer-bg.jpg);
+}
+
+.right {
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    background-image: url(/src/assets/img/dc-logo-bg.png);
+}
+
+.container {
+    display: flex;
+}
+
+.container>* {
+    width: 50%;
+}
+
+.left {
+    min-height: 25rem;
+    padding: 1px;
+    display: flex;
+}
+
+.sec{
+    padding-right: 4rem;
+}
+
+.debug{
+    color: red;
+}
 </style>
--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,14 +1,33 @@
 <script>
 
-import NavSection from './NavSection.vue'
+// import NavSection from './NavSection.vue'
 // questo e l'header
 export default {
-    components: {
-        NavSection,
-  },
+
     data() {
         return {
-            // placeholder
+            comics: [
+                {
+                    "title": "DIGITAL COMICS",
+                    "img": "/src/assets/img/buy-comics-digital-comics.png"
+                },
+                {
+                    "title": "DC MERCHANDISE",
+                    "img": "/src/assets/img/buy-comics-merchandise.png"
+                },
+                {
+                    "title": "SUBSCRIPTION",
+                    "img": "/src/assets/img/buy-comics-shop-locator.png"
+                },
+                {
+                    "title": "COMIC SHOP LOCATOR",
+                    "img": "/src/assets/img/buy-comics-subscriptions.png"
+                },
+                {
+                    "title": "DC POWER VISA",
+                    "img": "/src/assets/img/buy-dc-power-visa.svg"
+                },
+            ]
         }
     },
     methods: {
@@ -27,11 +46,14 @@ export default {
 <template>
     <div class="cont">
         <div class="container">
-            <NavSection title="QQQQQ"></NavSection>
-            <NavSection></NavSection>
-            <NavSection></NavSection>
-            <NavSection></NavSection>
-            <NavSection></NavSection>
+            <!-- <NavSection icon="../assets/img/buy-dc-power-visa.svg" title="DIGITAL COMICS"></NavSection> -->
+            <section>
+
+                <span v-for="element in this.comics">
+                    <img :src="element.img" alt="">
+                    <div class="title">{{ element.title }}</div>
+                </span>
+            </section>
         </div>
     </div>
 </template>
@@ -39,17 +61,48 @@ export default {
 
 
 <style scoped>
-.cont{
+.cont {
     background: #0282f9;
     height: 156px;
     display: flex;
     justify-content: center;
     align-items: center;
 }
-.container{
-    border: 2px dashed red;
+
+.container {
+    /* border: 2px dashed red; */
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
+    width: 1100px;
+}
+
+span {
+    display: flex;
+    align-items: center;
+    /* border: 2px dashed green; */
+    width: 100%;
+    gap: 1rem;
+}
+
+section{
+    height: 70px;
+    display: flex;
+  
+}
+img {
+    width: 20%;
+}
+
+.container span:nth-child(3) img {
+    width: 15%;
+}
+
+span:last-child img {
+    width: 25%;
+}
+
+span:first-child img {
+    width: 20%;
 }
 </style>
