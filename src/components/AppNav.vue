64e23c335e30196627a70dd272522e16c01bc491
Vladislav Ciuperca <ciuperca37@gmail.com>
2025-04-03 11:22:15 +0200

    insrisco immagini

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,14 +1,32 @@
 <script>
 
-import NavSection from './NavSection.vue'
 // questo e l'header
 export default {
-    components: {
-        NavSection,
-  },
     data() {
         return {
-            // placeholder
+            sections: [
+                {
+                    title: "DIGITALCOMICS",
+                    image: "/src/assets/img/buy-comics-digital-comics.png"
+                },
+                {
+                    title: "DC MERCHANDISE",
+                    image: "/src/assets/img/buy-comics-merchandise.png"
+                },
+                {
+                    title: "SUBSCRIPTION",
+                    image: "/src/assets/img/buy-comics-subscriptions.png"
+                },
+                {
+                    title: "COMIC SHOP LOCATOR",
+                    image: "/src/assets/img/buy-comics-shop-locator.png"
+                },
+                {
+                    title: "DC POWER VISA",
+                    image: "/src/assets/img/buy-comics-digital-comics.png"
+                }
+            ]
+
         }
     },
     methods: {
@@ -27,29 +45,43 @@ export default {
 <template>
     <div class="cont">
         <div class="container">
-            <NavSection title="QQQQQ"></NavSection>
-            <NavSection></NavSection>
-            <NavSection></NavSection>
-            <NavSection></NavSection>
-            <NavSection></NavSection>
+            <div class="carta" v-for="section in sections">
+                <img :src="section.image" alt="">
+                {{ section.title }}
+            </div>
         </div>
     </div>
 </template>
 <!-- ///////////////////////////////////////////// -->
 
 
+
 <style scoped>
-.cont{
+.cont {
     background: #0282f9;
     height: 156px;
     display: flex;
     justify-content: center;
     align-items: center;
 }
-.container{
+
+.container {
     border: 2px dashed red;
     display: flex;
     justify-content: center;
     align-items: center;
+}
+
+.carta {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 50%;
+    border: 2px dashed red
+}
+
+img {
+    height: 40%;
+    width: 30%;
 }
 </style>
