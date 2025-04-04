52b040b026efeebb2a76bbde856bd4d85055548d
Vladislav-Ciuperca <ciuperca37@gmail.com>
2025-04-04 23:04:32 +0200

    creo card

---
+++ src/components/content-components/ContentCard.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    props: {
+        thumb: 'Object',
+        price: 'string',
+    }
+}
+</script>
+
+<template>
+
+    <div class="card">
+        <img class="rounded" :src="thumb" alt="">
+
+        <span class="badge text-bg-danger price ">{{ price }}</span>
+    </div>
+</template>
+
+
+<style scoped>
+.card {
+    position: relative;
+    cursor: pointer;
+}
+
+.price {
+    position: absolute;
+    top: 3%;
+    right: 5%;
+}
+</style>
